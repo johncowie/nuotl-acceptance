b81853a7b82c5293c7fb1c61530587e5f56513d3
John Cowie <john.a.cowie@gmail.com>
2013-07-02 19:00:51 +0100

    Initial commit

---
+++ src/nuotl_acceptance/core.clj
@@ -0,0 +1,6 @@
+(ns nuotl-acceptance.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/nuotl_acceptance/core_test.clj
@@ -0,0 +1,7 @@
+(ns nuotl-acceptance.core-test
+  (:use clojure.test
+        nuotl-acceptance.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
