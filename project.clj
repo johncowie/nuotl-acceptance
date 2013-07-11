33f6cf92cb52469798bb6dc8dd1cda96f5fa2038
John Cowie <john.a.cowie@gmail.com>
2013-07-11 18:18:57 +0100

    Added dependencies for webdriver and twitter, and started developing the test.

--- project.clj
+++ project.clj
@@ -3,4 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]])
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [clj-webdriver "0.6.0"]
+                 [org.twitter4j/twitter4j-core "3.0.3"]
+                 ])
--- src/nuotl_acceptance/core.clj
+++ src/nuotl_acceptance/core.clj
@@ -1,6 +1,13 @@
-(ns nuotl-acceptance.core)
+(ns nuotl-acceptance.core
+  (:require [clj-webdriver.taxi :as taxi]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn account-name "In future should get this from twitter (based on credentials used)"
+  []
+  "@nuotl")
+
+(defn site-location [] "http://preview.nextupontheleft.org/")
+
+(def success-tweet
+  (format "%s 2/3/2014 9pm 3h N An event is happening at www.somewhere.com" (account-name)))
+
+(taxi/set-driver! {:browser :firefox} "http://preview.nextupontheleft.org/events/2013/3")
