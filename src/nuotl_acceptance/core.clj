5e362cadc3cef251695fa32a81b44fa492d42688
John Cowie <john.a.cowie@gmail.com>
2013-08-03 16:20:24 +0100

    Test now checks that reply is deleted when tweet is deleted.

--- src/nuotl_acceptance/core.clj
+++ src/nuotl_acceptance/core.clj
@@ -1,6 +1,6 @@
 (ns nuotl-acceptance.core
   (:require [clj-webdriver.taxi :as taxi]
-            [midje.sweet :refer [facts =>]]
+            [midje.sweet :refer [facts fact =>]]
             [clj-yaml.core :as yaml]
             [clj-time.core :as t]
             [clj-time.local :as lt]
@@ -23,25 +23,26 @@
   (println "Sending message: " message)
   (. twitter (updateStatus (StatusUpdate. message))))
 
-(defn get-most-recent-tweet-text []
+(defn get-most-recent-tweet []
   (let [status (nth (. twitter (getMentionsTimeline)) 0)]
     (println (. status (getCreatedAt)))
-    (. status (getText))))
+    status))
 
-(defn check-for-message [message]
-  (loop [interval 2000 attempts 1 counter 0]
-    (do
-      (Thread/sleep interval)
-      (let [most-recent (get-most-recent-tweet-text)]
-        (if (= counter attempts)
-          false
-          (do
-            (println most-recent)
-            (println message)
-            (if (nil? (re-find (re-pattern message) most-recent))
-              (recur interval attempts (inc counter))
-              true
-              )))))))
+(defn text-contains-str? [tweet message]
+  (do
+    (println (. tweet (getText)))
+    (println message)
+    (not (nil? (re-find (re-pattern message) (. tweet (getText)))))))
+
+(defn url-contains-str? [tweet string]
+  (let [urls (. tweet (getURLEntities))]
+    (if (> (count urls) 0)
+      (let [url (. (nth urls 0) (getExpandedURL))]
+        (println string)
+        (println url)
+        (not (nil? (re-find (re-pattern string) url))))
+      false
+      )))
 
 (taxi/set-driver! {:browser (keyword (config :browser))})
 
@@ -59,17 +60,25 @@
                                        (str day "/" month "/" year)
                                        (str hour ":" minutes)
                                        random))]
-         (check-for-message "Your event tweet was successful!") => true
+         (Thread/sleep 1000)
+         (let [response (get-most-recent-tweet)]
+           (fact "Last tweet sent contains success message"
+                 (text-contains-str? response "Your event tweet was successful!") => true)
+           (fact "Last tweet has url with id of event tweet"
+                 (url-contains-str? response (str (. tweet (getId)))) => true))
          (taxi/to   (format "%s/events/%s/%s" (config :frontend) year month))
-         (taxi/text (taxi/find-element {:css (format "#E%s .time" (. tweet (getId))) })) => (str hour ":" minutes)
-         (taxi/text (taxi/find-element {:css (format "#E%s .text" (. tweet (getId))) })) => (str "An event bbc.co.uk " random)
-         (taxi/text (taxi/find-element {:css (format "#E%s .name" (. tweet (getId))) })) => "Next Up Test User"
-         (taxi/text (taxi/find-element {:css (format "#E%s .area" (. tweet (getId))) })) => "North London"
+         (fact (taxi/text (taxi/find-element {:css (format "#E%s .time" (. tweet (getId))) })) => (str hour ":" minutes))
+         (fact (taxi/text (taxi/find-element {:css (format "#E%s .text" (. tweet (getId))) })) => (str "An event bbc.co.uk " random))
+         (fact (taxi/text (taxi/find-element {:css (format "#E%s .name" (. tweet (getId))) })) => "Next Up Test User")
+         (fact (taxi/text (taxi/find-element {:css (format "#E%s .area" (. tweet (getId))) })) => "North London")
          (. twitter (destroyStatus (. tweet (getId))))
          (debug "Made destroyStatus call - now sleeping for 1000ms")
          (Thread/sleep 1000)
          (taxi/refresh)
-         (taxi/exists? (format "#E%s" (. tweet (getId)))) => false
+         (fact "Event no longer exists on page. " (taxi/exists? (format "#E%s" (. tweet (getId)))) => false)
+         (let [response (get-most-recent-tweet)]
+           (fact "Last tweet sent no longer has url with id of event tweet"
+                 (url-contains-str? response (str (. tweet (getId)))) => false))
          ))))
 
 (taxi/quit)
